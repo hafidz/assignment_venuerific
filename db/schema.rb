33a3f2c21ba9b1a44cad37d9e152391dd0eec39b
Hafiz Husein <opattujuh47@gmail.com>
2021-05-20 16:46:00 +0700

    update database to posgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,10 @@
 
 ActiveRecord::Schema.define(version: 2021_05_19_203227) do
 
-  create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
     t.integer "user_id"
     t.string "title"
     t.text "content"
@@ -20,7 +23,7 @@ ActiveRecord::Schema.define(version: 2021_05_19_203227) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
